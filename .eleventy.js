4c66f898dd3ee53c6800912c5fb37c82b307a431
Eli Levit <kalach2010@gmail.com>
2020-01-31 12:54:09 +0200

    Add globals and about page

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
-module.exports = function (config) {
+module.exports = function(config) {
   config.addPassthroughCopy('src/styles');
   config.addPassthroughCopy('src/assets');
   config.addPassthroughCopy('src/fonts');
   config.addPassthroughCopy('src/index.js');
   config.addPassthroughCopy('src/scripts');
-  config.addPassthroughCopy({ "src/other": "/" });
+  config.addPassthroughCopy({ 'src/other': '/' });
 
   return {
     dir: {
@@ -12,6 +12,7 @@ module.exports = function (config) {
       output: '_site',
       includes: 'includes',
       layouts: 'layouts',
+      data: 'global',
     },
     dataTemplateEngine: 'njk',
     markdownTemplateEngine: false,
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-let toggle = document.querySelector('.theme-toggler');
+const toggle = document.querySelector('.theme-toggler');
 
 if (localStorage.getItem('theme') === 'light') {
   document.body.classList.add('light');
@@ -7,6 +7,8 @@ if (localStorage.getItem('theme') === 'light') {
 toggle.addEventListener('click', function(e) {
   e.preventDefault();
 
+  document.body.style.transition = 'background-color 0.7s ease-in-out';
+
   if (document.body.classList.contains('light')) {
     document.body.classList.remove('light');
     localStorage.setItem('theme', 'dark');
@@ -14,4 +16,30 @@ toggle.addEventListener('click', function(e) {
     document.body.classList.add('light');
     localStorage.setItem('theme', 'light');
   }
+
+  setTimeout(() => {
+    document.body.style.transition = '';
+  }, 1000);
 });
+
+!(function() {
+  let e, t;
+  function n(e) {
+    return !(0 !== e.button || e.metaKey || e.ctrlKey || e.shiftKey || e.altKey);
+  }
+  document.addEventListener('mousedown', n => {
+    (e = (function e(t) {
+      return 'A' === t.tagName ||
+        'BUTTON' === t.tagName ||
+        ('INPUT' === t.tagName && 'submit' === t.type)
+        ? t
+        : t.parentElement
+        ? e(t.parentElement)
+        : null;
+    })(n.target)),
+      (t = Date.now());
+  }),
+    document.addEventListener('focusin', n => {
+      n.target === e && Date.now() - t < 999 && n.target.blur();
+    });
+})();
