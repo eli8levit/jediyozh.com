421bcb588a18d597ca219b479c2b74c6aaf1d591
Eli Levit <kalach2010@gmail.com>
2020-02-18 01:01:59 +0200

    Changes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -22,24 +22,15 @@ toggle.addEventListener('click', function(e) {
   }, 1000);
 });
 
-!(function() {
-  let e, t;
-  function n(e) {
-    return !(0 !== e.button || e.metaKey || e.ctrlKey || e.shiftKey || e.altKey);
+let lastDown, lastDownAt;
+
+document.addEventListener('mousedown', e => {
+  lastDown = e.target;
+  lastDownAt = Date.now();
+});
+
+document.addEventListener('focusin', e => {
+  if (e.target === lastDown && Date.now() - lastDownAt < 999) {
+    e.target.blur();
   }
-  document.addEventListener('mousedown', n => {
-    (e = (function e(t) {
-      return 'A' === t.tagName ||
-        'BUTTON' === t.tagName ||
-        ('INPUT' === t.tagName && 'submit' === t.type)
-        ? t
-        : t.parentElement
-        ? e(t.parentElement)
-        : null;
-    })(n.target)),
-      (t = Date.now());
-  }),
-    document.addEventListener('focusin', n => {
-      n.target === e && Date.now() - t < 999 && n.target.blur();
-    });
-})();
+});
