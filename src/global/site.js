bff21f0a3c446f3f71ab4c120a81fa69c5be920f
Eli Levit <kalach2010@gmail.com>
2020-03-23 20:59:25 +0200

    Add dynamic title; add content

--- src/global/site.js
+++ src/global/site.js
@@ -1,8 +1,18 @@
 module.exports = {
   en: {
-    naming: 'I’m Eli Levit',
+    name: 'I’m Eli Levit',
+    homeTitle: 'Eli Levit',
+    profession: 'Front-end Developer',
+    info:
+      'I’m trying to write posts. Also I love to make videos, photos and code some stuff. Also I like read books, wine and girls. A11y, React and super cool console.log debugging.',
+    moreAboutMe: 'More about me',
   },
   ru: {
-    naming: 'Я Эли Левит',
+    name: 'Я Эли Левит',
+    homeTitle: 'Эли Левит',
+    profession: 'Фронтенд Разработчик',
+    info:
+      'Я пытаюсь писать посты. Еще я люблю делать видео, снимать и кодить. Еще я люблю читать книги, вино и девочек. Доступность, Реакт и очень крутая консольная отладка.',
+    moreAboutMe: 'Больше обо мне',
   },
 };
