b264cdc4c394101f3a75df693b9bce5bdfa6f663
Eli Levit <kalach2010@gmail.com>
2020-06-28 20:59:17 +0300

    Make script inline

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,68 +1,73 @@
-const toggle = document.querySelector('.theme-toggler');
-const root = document.documentElement;
-const transition = root.style.getPropertyValue('--theme-color-transition');
+(function () {
+  const root = document.documentElement;
+  const transition = root.style.getPropertyValue('--theme-color-transition');
 
-root.style.setProperty('--theme-color-transition', 'none');
+  root.style.setProperty('--theme-color-transition', 'none');
 
-const setDark = () => document.body.classList.remove('light');
-const setLight = () => document.body.classList.add('light');
+  const setDark = () => document.body.classList.remove('light');
+  const setLight = () => document.body.classList.add('light');
 
-toggle.addEventListener('click', function (e) {
-  e.preventDefault();
+  document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.querySelector('.theme-toggler');
+    toggle.addEventListener('click', function (e) {
+      e.preventDefault();
 
-  if (document.body.classList.contains('light')) {
-    setDark();
-    sessionStorage.setItem('theme', 'dark');
-  } else {
-    setLight();
-    sessionStorage.setItem('theme', 'light');
+      if (document.body.classList.contains('light')) {
+        setDark();
+        sessionStorage.setItem('theme', 'dark');
+      } else {
+        setLight();
+        sessionStorage.setItem('theme', 'light');
+      }
+    });
+  });
+
+  function getPreferedColorScheme() {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return 'dark';
+    }
+    return 'light';
   }
-});
 
-function getPreferedColorScheme() {
-  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    return 'dark';
+  function setTheme(theme) {
+    switch (theme) {
+      case 'dark':
+        setDark();
+        break;
+      default:
+        setLight();
+    }
   }
-  return 'light';
-}
 
-function setTheme(theme) {
-  switch (theme) {
-    case 'dark':
-      setDark();
-      break;
-    default:
-      setLight();
+  function calculateNextTheme() {
+    const theme = sessionStorage.getItem('theme');
+    if (theme !== null) {
+      setTheme(theme);
+    } else {
+      setTheme(getPreferedColorScheme());
+    }
+    setTimeout(() => {
+      root.style.setProperty('--theme-color-transition', transition);
+    }, 100);
   }
-}
 
-function calculateNextTheme() {
-  const theme = sessionStorage.getItem('theme');
-  if (theme !== null) {
-    setTheme(theme);
-  } else {
-    setTheme(getPreferedColorScheme());
+  if (window.matchMedia) {
+    console.log('match media');
+    const colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    calculateNextTheme();
+    colorSchemeQuery.addListener(calculateNextTheme);
   }
-  setTimeout(() => {
-    root.style.setProperty('--theme-color-transition', transition);
-  }, 100);
-}
 
-if (window.matchMedia) {
-  const colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
-  calculateNextTheme();
-  colorSchemeQuery.addListener(calculateNextTheme);
-}
+  let lastDown, lastDownAt;
 
-let lastDown, lastDownAt;
+  document.addEventListener('mousedown', (e) => {
+    lastDown = e.target;
+    lastDownAt = Date.now();
+  });
 
-document.addEventListener('mousedown', (e) => {
-  lastDown = e.target;
-  lastDownAt = Date.now();
-});
-
-document.addEventListener('focusin', (e) => {
-  if (e.target === lastDown && Date.now() - lastDownAt < 999) {
-    e.target.blur();
-  }
-});
+  document.addEventListener('focusin', (e) => {
+    if (e.target === lastDown && Date.now() - lastDownAt < 999) {
+      e.target.blur();
+    }
+  });
+})();
