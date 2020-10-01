6b2d2935f4dee0190174e956d567751c9a5f73b5
Eli Levit <kalach2010@gmail.com>
2020-10-01 23:50:23 +0300

    Change the structure of the site confgi

--- src/global/site.js
+++ src/global/site.js
@@ -10,31 +10,47 @@ module.exports = {
     },
   ],
   en: {
-    greeting: 'Eli Levit',
-    homeTitle: 'Eli Levit',
-    aboutTitle: 'About me',
-    profession: 'Front-end Developer',
-    shortBio:
-      'The only programmer in Israel who knows CSS besides React and thinks about accessibility and performance. I can move my ears. Dream to do something useful.',
-    moreAboutMe: 'More about me',
-    headerHome: 'Home',
-    headerBlog: 'Blog',
-    headerWork: 'Work',
-    headerAboutMe: 'About',
+    pageTitles: {
+      home: 'Eli Levit',
+      blog: 'Blog of Eli',
+      work: 'Work of Eli',
+      about: 'About Eli',
+    },
+    header: {
+      home: 'Home',
+      blog: 'Blog',
+      work: 'Work',
+      about: 'About',
+    },
+    homePage: {
+      greeting: 'Eli Levit',
+      profession: 'Front-end Developer',
+      shortBio:
+        'The only programmer in Israel who knows CSS besides React and thinks about accessibility and performance. I can move my ears. Dream to do something useful.',
+      moreAboutMe: 'More about me',
+    },
     footerCreatedWith: 'Created with',
   },
   ru: {
-    greeting: 'Эли Левит',
-    homeTitle: 'Эли Левит',
-    aboutTitle: 'Обо мне',
-    profession: 'Фронтенд Разработчик',
-    shortBio:
-      'Единственный программист в Израиле, который знает CSS помимо React и думает о доступности и производительности. Умею двигать ушами. Мечтаю сделать что-то полезное.',
-    moreAboutMe: 'Больше обо мне',
-    headerHome: 'Главная',
-    headerBlog: 'Блог',
-    headerWork: 'Проекты',
-    headerAboutMe: 'Обо мне',
+    pageTitles: {
+      home: 'Эли Левит',
+      blog: 'Блог Эли',
+      work: 'Проекты Эли',
+      about: 'Об Эли',
+    },
+    header: {
+      home: 'Главная',
+      blog: 'Блог',
+      work: 'Проекты',
+      about: 'Обо мне',
+    },
+    homePage: {
+      greeting: 'Эли Левит',
+      profession: 'Фронтенд Разработчик',
+      shortBio:
+        'Единственный программист в Израиле, который знает CSS помимо React и думает о доступности и производительности. Умею двигать ушами. Мечтаю сделать что-то полезное.',
+      moreAboutMe: 'Больше обо мне',
+    },
     footerCreatedWith: 'Сделано с',
   },
 };
